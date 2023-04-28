9f5d65ffebbca3e9629aca76987dae122d5a55c9
manieshkumar <itzmemaniesh.asd@gmail.com>
2023-04-28 16:24:04 +0530

    - added in comments

--- src/com/java8/date/api/DateTime.java
+++ src/com/java8/date/api/DateTime.java
@@ -7,7 +7,7 @@ public class DateTime {
 
 	public static void main(String[] args) {
 		
-		//create localdate instance
+		//create local-date instance
 		LocalDate date=LocalDate.now();
 		System.out.println(date);
 		
